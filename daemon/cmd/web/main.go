ccf57bbc1c47f999fd22c31299f301f50efd7613
Hideaki Ibuki <ibuki-90@hotmail.co.jp>
2020-07-02 00:06:20 +0900

    add echo and swag

--- daemon/cmd/web/main.go
+++ daemon/cmd/web/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main()  {
-	println("aaa")
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/swaggo/echo-swagger"
+	"net/http"
+
+	_ "github.com/ikihiki/cremona/daemon/cmd/web/docs"
+)
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample swagger server.
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:32424
+// @BasePath /api/v1
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", redirect_swagger)
+	e.GET("/api/v1/test", hello)
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":32424"))
+}
+
+// test
+// @Summary test
+// @Description get testdata
+// @Success 200 {object} string
+// @Router /test [get]
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func redirect_swagger(c echo.Context) error  {
+	return c.Redirect(http.StatusMovedPermanently, "/swagger/index.html")
 }
--- daemon/go.mod
+++ daemon/go.mod
@@ -3,12 +3,24 @@ module github.com/ikihiki/cremona/daemon
 go 1.13
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/go-openapi/spec v0.19.8 // indirect
+	github.com/go-openapi/swag v0.19.9 // indirect
+	github.com/labstack/echo/v4 v4.1.16
+	github.com/mailru/easyjson v0.7.1 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/mattn/go-mastodon v0.0.4
 	github.com/sourcegraph/jsonrpc2 v0.0.0-20200429184054-15c2290dcb37
 	github.com/stretchr/testify v1.6.1
+	github.com/swaggo/echo-swagger v1.0.0 // indirect
+	github.com/swaggo/swag v1.6.7
+	github.com/valyala/fasttemplate v1.1.1 // indirect
 	github.com/vishvananda/netlink v1.1.0
 	github.com/vishvananda/netns v0.0.0-20200520041808-52d707b772fe
 	github.com/vmihailenco/msgpack v4.0.4+incompatible
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
 	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae
+	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/tools v0.0.0-20200701041122-1837592efa10 // indirect
 	gopkg.in/yaml.v2 v2.3.0
 )
