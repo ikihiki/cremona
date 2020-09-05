92b9e6d2aacd9992c77fd9902e82f41f99633dee
Hideaki Ibuki <ibuki-90@hotmail.co.jp>
2020-09-05 16:13:24 +0900

    Add buffer define

--- driver/src/buffer.h
+++ driver/src/buffer.h
@@ -1,4 +1,10 @@
 typedef struct crmna_buf {
   char *buf;
   size_t buf_size;
+  size_t used_size;
 } crmna_buf_t;
+
+#define DEFINE_CRMNA_BUF(name, size)                                           \
+  char name##_buffer_content[size];                                            \
+  crmna_buf_t name = {                                                         \
+      .buf = name##_buffer_content, .buf_size = size, .used_size = 0};
